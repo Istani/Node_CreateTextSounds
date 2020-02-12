12c6e53f758ae96e3fa53eb73350f1d963b65d28
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2020-02-12 23:36:59 +0100

    sic

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ console.log("=".repeat(software.length));
 // WORKS NOT ON LINUX... WHY?
 const say = require("say");
 
-say.export("Never gonna give you up.", null, 1, "tmp/Never gonna give you up.wav");
+//say.export("Never gonna give you up.", null, 1, "tmp/Never gonna give you up.wav");
 
 say.export("Was fuer ein geiles Game! Das klingt nach einer Runde Teemo Smite!", null, 1, "tmp/gege.wav");
 say.export("Zero Peel!", null, 1, "tmp/peel.wav");
